98873e00ac1e16c926a382417318da3b435cbe2e
Fihri Aziz <fihriaziz@gmail.com>
2023-02-08 15:03:44 +0700

    fix: reusable component applicationLogo

--- resources/js/Shared/GuestLayout.vue
+++ resources/js/Shared/GuestLayout.vue
@@ -9,7 +9,7 @@ export default {
   <div id="background" class="font-sans antialiased auth-bg">
     <div class="text-gray-700">
       <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
-        <ApplicationLogo />
+        <ApplicationLogo width="291" height="56" />
         <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
           <slot />
         </div>
