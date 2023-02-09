4d65b5e55f7fcf05d6d553a4b0777e0e55b45946
Fihri Aziz <fihriaziz@gmail.com>
2023-02-09 15:16:17 +0700

    fix: notifications lang

---
+++ resources/lang/en/notifications.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'submit' => 'Data has been created',
+    'update' => 'Data has been updated',
+    'delete' => [
+        'question' => 'Are you sure delete?',
+        'answer' => 'Yes, delete',
+        'info' => 'Deleted',
+        'message' => 'Data has been deleted',
+    ],
+
+    'fields' => [],
+];
---
+++ resources/lang/id/notifications.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'submit' => 'Data berhasil dibuat',
+    'update' => 'Data berhasil diperbaharui',
+    'delete' => [
+        'question' => 'Apakah yakin akan dihapus?',
+        'answer' => 'Ya, hapus',
+        'info' => 'Terhapus',
+        'message' => 'Data berhasil dihapus',
+    ],
+
+    'fields' => [],
+];
