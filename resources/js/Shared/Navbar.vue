09b078081a23b03bc4800983a1a6c35785c38628
Fihri Aziz <fihriaziz@gmail.com>
2023-02-21 12:03:48 +0700

    fix: button as link

--- resources/js/Shared/Navbar.vue
+++ resources/js/Shared/Navbar.vue
@@ -72,7 +72,8 @@ export default {
             </Link>
             <Link
               class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100
-                            focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
+                            focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out
+                            flex gap-1 w-full cursor-pointer"
               href="/logout" method="post" as="button"
             >
               {{ $t('auth.actions.logout') }}
